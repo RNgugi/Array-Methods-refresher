ace0f22083ad82e390e8a1ab9608bf57667ca13b
RNgugi <reubeenkihiu@gmail.com>
2020-06-17 18:13:06 +0300

    doubling money

--- script.js
+++ script.js
@@ -26,6 +26,15 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+//double everyones money
+function doubleMoney(){
+    data = data.map ((user) => {
+        return{...user, money: user.money*2}
+    });
+
+    updateDOM();
+}
+
 //Add new object to data array
 function addData(obj) {
   data.push(obj);
@@ -53,6 +62,6 @@ function formatMoney(number) {
   return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
-
 //event listeners
 addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
